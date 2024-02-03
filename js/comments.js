61088712df388353789f9738879271b330efc338
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-02-03 01:52:03 -0500

    Create comments.js

---
+++ js/comments.js
@@ -0,0 +1,17 @@
+//  This  is  a  single-line  comment
+let  x  =  42;  //  This  is  also  a  single  line  comment,  although  the  part  before  the  double  slash  is  proper  code  and  will  be  executed
+//  This  line  and  the  next  one  will  be  ignored
+//  x  =  8;
+console.log(x);  //  ->  42
+
+/*
+         This  is  a  block
+         comment  and  it  can
+         span  multiple  lines
+   
+         So  this  code  won't  be  executed
+         console.log("Hello,  World!");
+*/
+   
+let  x  /*  because  no  better  name  */  =  42;
+console.log(x);
