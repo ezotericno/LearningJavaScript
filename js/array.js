412c359a10799abc6dbcbe931ed80f63b0a6bb55
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-02-03 01:43:59 -0500

    Update array.js

--- js/array.js
+++ js/array.js
@@ -8,3 +8,21 @@ console.log(days[0]);  //  ->  Sunday
    
 let  emptyArray  =  [];
 console.log(emptyArray[0]);  //  ->  undefined
+
+let  users  =[  
+         {
+                 name:  "Calvin",
+                 surname:  "Hart",
+                 age:  66,
+                 email:  "CalvinMHart@teleworm.us"
+         },
+         {
+                 name:  "Mateus",
+                 surname:  "Pinto",
+                 age:  21,
+                 email:  "MateusPinto@dayrep.com"
+         }
+];
+   
+console.log(users[0].name);  //  ->  Calvin
+console.log(users[1].age);  //  ->  21
