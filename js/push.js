2b2ca49316654447f578c6bd7ddd217148bb80bd
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-02-03 01:47:30 -0500

    Create push.js

---
+++ js/push.js
@@ -0,0 +1,7 @@
+let  names  =  ["Olivia",  "Emma",  "Mateo",  "Samuel"];
+console.log(names.length);  //  ->  4
+   
+names.push("Amelia");
+console.log(names.length);  //  ->  5
+console.log(names);  //  -  >  ["Olivia",  "Emma",  "Mateo",  
+"Samuel",  "Amelia"]
